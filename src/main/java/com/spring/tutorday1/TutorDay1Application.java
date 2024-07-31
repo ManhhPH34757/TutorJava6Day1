3ff734d32e1ca580be52ac9922db9d8eef5f651d
ManhhPH34757 <nguyenmanh2707ngt@gmail.com>
2024-07-31 20:38:33 +0700

    create project

---
+++ src/main/java/com/spring/tutorday1/TutorDay1Application.java
@@ -0,0 +1,13 @@
+package com.spring.tutorday1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorDay1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/tutorday1/TutorDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tutorday1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorDay1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
